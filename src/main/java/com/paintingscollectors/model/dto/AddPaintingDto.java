5e8bb51fa3413432395927fa0aef1f7eba430953
Stoyan85Stoyanov <stoyan.stoyanov.kz@mail.bg>
2025-10-17 20:50:43 +0300

    PaintingsCollectors-App

---
+++ src/main/java/com/paintingscollectors/model/dto/AddPaintingDto.java
@@ -0,0 +1,28 @@
+package com.paintingscollectors.model.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Getter
+@Setter
+public class AddPaintingDto {
+
+    @NotBlank
+    @Size(min = 5, max = 40)
+    private String name;
+
+    @NotBlank
+    @Size(min = 5, max = 40)
+    private String author;
+
+    @NotBlank
+    @Size(max = 150)
+    private String imageUrl;
+
+
+    @NotBlank(message = "You must select a style!")
+    private String style;
+}
