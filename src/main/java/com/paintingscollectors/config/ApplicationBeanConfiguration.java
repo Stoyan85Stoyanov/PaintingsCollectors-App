0cd6cc3cde79b8f6484eab134a8762e45908fb4e
Stoyan85Stoyanov <stoyan.stoyanov.kz@mail.bg>
2025-10-18 21:16:44 +0300

    PaintingsCollectors-App

---
+++ src/main/java/com/paintingscollectors/config/ApplicationBeanConfiguration.java
@@ -0,0 +1,31 @@
+package com.paintingscollectors.config;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class ApplicationBeanConfiguration {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public Gson gson() {
+        return new GsonBuilder()
+                .excludeFieldsWithoutExposeAnnotation()
+                .setPrettyPrinting()
+                .create();
+    }
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+}
