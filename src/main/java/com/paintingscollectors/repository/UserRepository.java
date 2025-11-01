1d3c84d3f9d6887bece5883ca127d0172e8b533d
Stoyan85Stoyanov <stoyan.stoyanov.kz@mail.bg>
2025-11-01 11:17:54 +0200

    PaintingsCollectors-App

---
+++ src/main/java/com/paintingscollectors/repository/UserRepository.java
@@ -0,0 +1,22 @@
+package com.paintingscollectors.repository;
+
+import com.paintingscollectors.model.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+    Optional<User> findByUsername(String username);
+
+    Optional<User> findByUsernameOrEmail(String username, String email);
+
+    Optional<User> findOptionalByUsername(String username);
+
+    Optional<User> findById(UUID id);
+
+
+}
