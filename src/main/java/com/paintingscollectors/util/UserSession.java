934908652dc67dc277f50a6e7e51ac91d8cd61db
Stoyan85Stoyanov <stoyan.stoyanov.kz@mail.bg>
2026-01-04 22:29:22 +0200

    PaintingsCollectors-App

--- src/main/java/com/paintingscollectors/util/UserSession.java
+++ src/main/java/com/paintingscollectors/util/UserSession.java
@@ -11,28 +11,28 @@ import java.util.UUID;
 @Getter
 public class UserSession {
 
-     private UUID id;
-     private String username;
+    private UUID id;
+    private String username;
 
-     public void login(UUID id, String username) {
+    public void login(UUID id, String username) {
         this.id = id;
         this.username = username;
-     }
-
-     public boolean isLoggedIn() {
-         return id != null;
-     }
-
-     public UUID id() {
-         return id;
     }
 
-     public String username() {
-         return username;
+    public boolean isLoggedIn() {
+        return id != null;
     }
 
-     public void logout() {
-         id = null;
-         username = null;
+    public UUID id() {
+        return id;
+    }
+
+    public String username() {
+        return username;
+    }
+
+    public void logout() {
+        id = null;
+        username = null;
     }
 }
